52bf82178ca175848a922aa4df1f07d69ad60441
syedmehedi0003 <mehedihasanbahar.com>
2022-05-28 12:51:49 +0600

    new commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { get } = require('express/lib/response');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,6 +22,9 @@ async function run() {
         await client.connect();
         const productCollection = client.db('laptop-parts').collection('product');
         const userCollection = client.db('laptop-parts').collection('user');
+        const profileCollection = client.db('laptop-parts').collection('profile');
+        const orderCollection = client.db('laptop-parts').collection('order');
+        const reviewCollection = client.db('laptop-parts').collection('review');
 
         app.get('/product', async (req, res) => {
             const query = {};
@@ -53,33 +57,126 @@ async function run() {
 
 
         //Update
-        app.put('/product/:id', async (req, res) => {
+        app.patch('/product/:id', async (req, res) => {
             const id = req.params.id;
+
             const updatedData = req.body;
+            console.log(id);
             const filter = { _id: ObjectId(id) };
 
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
                     // quantity: updatedData.quantity,
-                    quantity: updatedData.newQuantity,
-
+                    available: updatedData.newQuantity,
 
                 }
             };
 
             const result = await productCollection.updateOne(filter, updateDoc, options);
+
             res.send(result);
 
         });
+
+        //Post Order
+        app.post('/order', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result2 = await orderCollection.insertOne(newProduct);
+            res.send(result2);
+        });
+
+        //Get order
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user1 = await orderCollection.find(query).toArray();
+            res.send(user1);
+        });
+
+
+        //Get review
+        app.get('/review', async (req, res) => {
+            // const email = req.query.email;
+            // const query = { email: email };
+            // const user = await reviewCollection.find(query).toArray();
+            // res.send(user);
+
+            const users = await reviewCollection.find().toArray();
+            res.send(users);
+
+        });
+
+
+        //Get order
+        // app.get('/order', async (req, res) => {
+        //     const query = {};
+        //     const cursor = orderCollection.find(query);
+        //     const order = await cursor.toArray();
+        //     res.send(order);
+        // });
 
 
         //Post User
-        app.post('/profile', async (req, res) => {
-            const newProduct = req.body;
-            const result = await userCollection.insertOne(newProduct);
+        // app.post('/user', async (req, res) => {
+        //     const newProduct = req.body;
+        //     const result = await userCollection.insertOne(newProduct);
+        //     res.send(result);
+        // });
+
+        app.put('/user/profile', async (req, res) => {
+            const data = req.body;
+            const filter = { email: data.email };
+            console.log(filter);
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: data,
+            }
+            const result = await profileCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         });
+
+
+        //get User
+        // app.get('/user/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await userCollection.findOne(query);
+        //     res.send(result);
+        // })
+
+        //get User
+        app.get('/user', async (req, res) => {
+
+            // const email = req.query.email;
+            // const query = { email: email };
+            // const users = await userCollection.find(query).toArray();
+            // res.send(users);
+
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            // const requester = req.decoded.email;
+            // const requesterAccount = await userCollection.findOne({ email: requester });
+            // if (requesterAccount.role === 'admin') {
+            const filter = { email: email };
+            // const options = { upsert: true };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+            // }
+            // else {
+            //     res.status(403).send({ message: 'forbidden' });
+            // }
+
+        });
+
 
     }
 
